3d8453d84951593945316076d5c10696a8927057
vensky <ivan.fedotovskikh@gmail.com>
2024-09-09 20:32:20 +0500

    feat: add pages

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,30 @@
 'use strict';
+
+const initScrollProf = () => {
+    const btn = document.querySelector('.hero__button');
+    const link = document.querySelector('.nav__link--prof');
+    const section = document.querySelector('#prof');
+
+    btn.addEventListener('click', (e) => {
+        e.preventDefault();
+        if (section !== null) section.scrollIntoView({ behavior: "smooth" })
+    });
+
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+        if (section !== null) section.scrollIntoView({ behavior: "smooth" })
+    });
+}
+
+const initScrollFooter = () => {
+    const btn = document.querySelector('.nav__link--footer');
+    const section = document.querySelector('#footer');
+
+    btn.addEventListener('click', (e) => {
+        e.preventDefault();
+        if (section !== null) section.scrollIntoView({ behavior: "smooth" })
+    });
+}
+
+initScrollProf();
+initScrollFooter();
